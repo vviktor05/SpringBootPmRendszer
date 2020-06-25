ba684c8ad29d940bb0dcfed3952cc659aa01a2ce
Gyula <teszt@gmail.com>
2020-06-25 19:38:46 +0200

    Project service létrehozása és 2 alapvető függvény implementálása

---
+++ src/main/java/com/pmrendszer/service/ProjectService.java
@@ -0,0 +1,26 @@
+package com.pmrendszer.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.pmrendszer.domain.Project;
+import com.pmrendszer.repository.ProjectRepo;
+
+@Service
+public class ProjectService {
+	private ProjectRepo projectRepo;
+	private final int ACTIVE_STATUS_ID = 3;
+
+	public List<Project> getAllProjects(){
+		return projectRepo.findAll();
+	}
+	
+	public List<Project> getActiveProjects(){
+		return projectRepo.findByStatusId(ACTIVE_STATUS_ID);
+	}
+	
+	@Autowired
+	public void setProjectRepo(ProjectRepo projectRepo) {
+		this.projectRepo = projectRepo;
+	}
+}
